89c1a7e19a7135034e6957fcbf36e5958ae53ada
Achim Grolimund <ac.grolimund@gmail.com>
2025-01-05 19:02:55 +0000

    Remove initial schema migration files for database setup

--- migrations/20250104173813_initial_schema.down.sql
+++
@@ -1,6 +0,0 @@
--- Löschen der Tabellen in umgekehrter Reihenfolge
-DROP TABLE IF EXISTS print_settings;
-DROP TABLE IF EXISTS filament_spools;
-DROP TABLE IF EXISTS colors;
-DROP TABLE IF EXISTS materials;
-DROP TABLE IF EXISTS manufacturers;
