6e0c911acb9743c13adc56bcdeee2efefdbf1605
Achim Grolimund <ac.grolimund@gmail.com>
2025-01-08 20:41:55 +0000

    Refactor FilamentSpool model to use int type for IDs and add JSON tags for serialization

--- models/filament_spool.go
+++ models/filament_spool.go
@@ -1,22 +1,22 @@
 package models
 
 import (
-	"gorm.io/gorm"
 	"time"
+	"gorm.io/gorm"
 )
 
 type FilamentSpool struct {
 	gorm.Model
-	ManufacturerID  int16 `gorm:"not null"`
-	MaterialID      int16 `gorm:"not null"`
-	ColorID         int16 `gorm:"not null"`
-	WeightTotal     int   `gorm:"not null"`
-	WeightRemaining int
-	PurchaseDate    time.Time
-	Price           float64 `gorm:"type:decimal(10,2)"`
-	Rating          int16   `gorm:"check:rating BETWEEN 1 AND 5"`
-	Notes           string
-	Code            string       `gorm:"size:12;<-:false"` // Generated field, read-only
+	ManufacturerID  int          `gorm:"not null" json:"manufacturer_id"`
+	MaterialID      int          `gorm:"not null" json:"material_id"`
+	ColorID         int          `gorm:"not null" json:"color_id"`
+	WeightTotal     int          `gorm:"not null" json:"weight_total"`
+	WeightRemaining int          `gorm:"not null" json:"weight_remaining"`
+	PurchaseDate    time.Time    `gorm:"not null" json:"purchase_date"`
+	Price           float64      `gorm:"type:decimal(10,2)" json:"price"`
+	Rating          int16        `gorm:"check:rating BETWEEN 1 AND 5" json:"rating"`
+	Notes           string       `json:"notes"`
+	Code            string       `gorm:"not null" json:"code_id"`
 	Manufacturer    Manufacturer `gorm:"foreignKey:ManufacturerID"`
 	Material        Material     `gorm:"foreignKey:MaterialID"`
 	Color           Color        `gorm:"foreignKey:ColorID"`
